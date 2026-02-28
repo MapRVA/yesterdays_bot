129f1ef1f40b26c5a42dc15b53458fdd98c2a454
Mike O'Brien <mike@obrien.page>
2026-02-28 07:08:49 -0500

    Adjust subject serving frequency from 20 to 10 posts

--- the_bot.R
+++ the_bot.R
@@ -1,4 +1,4 @@
-# Serve a subject every 20 posts on average
-script <- sample(c("images.R", "subjects.R"), 1, prob = c(0.95, 0.05))
+# Serve a subject every 10 posts on average
+script <- sample(c("images.R", "subjects.R"), 1, prob = c(0.9, 0.1))
 
 source(script)
